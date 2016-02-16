df79fe509cd98857ba11797b5aec4b41a65a27d2
Michael Groen <mhg.groen@gmail.com>
2016-02-16 14:30:03 +0100

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 //include Gulp & plugins
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
 
-//created a task named log that uses gulp util
-gulp.task('log', function() {
-	gutil.log('Worfklows are awesome');
+//variable for the sources of the coffeescripts
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+//created a task named coffee that uses gulp-coffee to process coffeescript to js
+gulp.task('coffee', function() {
+	//refers to the original location for the processing 
+	gulp.src(coffeeSources)
+		//passes on the bare bare command 
+		.pipe(coffee({ bare: true})
+			//prevents gulp from stopping the executing when crashing and logs the error
+			.on('error', gutil.log))
+		//passes on the final destionation of the process
+		.pipe(gulp.dest('components/scripts'))
 });
